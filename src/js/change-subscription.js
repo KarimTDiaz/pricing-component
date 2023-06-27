b6b9c8832c2ff71b2c3c8fa72b7114039d3dc0d8
Karim Tilouni Diaz <117732405+KarimTDiaz@users.noreply.github.com>
2023-06-27 17:51:50 +0100

    Pricing component

--- src/js/change-subscription.js
+++ src/js/change-subscription.js
@@ -8,10 +8,10 @@ const changeModeSubscription = isChecked => {
   for (let index = 0; index < 3; index++) {
     for (let j = 0; j < 4; j++) {
       if (!isChecked) {
-        cardsElement.children[plans[index]].children[j + 1].textContent =
+        cardsElement.children[index].children[j + 1].textContent =
           Object.values(SUBSCRIPTION__OPTIONS.monthly[plans[index]])[j];
       } else {
-        cardsElement.children[plans[index]].children[j + 1].textContent =
+        cardsElement.children[index].children[j + 1].textContent =
           Object.values(SUBSCRIPTION__OPTIONS.annualy[plans[index]])[j];
       }
     }
